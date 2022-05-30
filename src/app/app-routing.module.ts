144c2259c7d18fda96194f398aa09848f3e828df
jossrc <kuro.rc10@gmail.com>
2022-05-29 22:21:06 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
 import { CrudEmpleadoComponent } from "./pages/crud-empleado/crud-empleado.component";
 import { ListaUsuariosComponent } from "./pages/lista-usuarios/lista-usuarios.component";
+import { CrudCategoriaPaqueteComponent } from "./pages/crud-categoria-paquete/crud-categoria-paquete.component";
+import { CrudVehiculoComponent } from "./pages/crud-vehiculo/crud-vehiculo.component";
+import { CrudLocalComponent } from "./pages/crud-local/crud-local.component";
 
 const routes: Routes = [
   { path: 'admin/home', component: AdminHomeComponent },
   { path: 'admin/empleados', component: CrudEmpleadoComponent },
-  { path: 'admin/categoria-paquete', component: ListaUsuariosComponent },
-  { path: 'admin/vehiculo', component: ListaUsuariosComponent },
-  { path: 'admin/local', component: ListaUsuariosComponent },
+  { path: 'admin/categoria-paquete', component: CrudCategoriaPaqueteComponent },
+  { path: 'admin/vehiculo', component: CrudVehiculoComponent },
+  { path: 'admin/local', component: CrudLocalComponent },
   { path: 'admin/clientes', component: ListaUsuariosComponent },
   { path: '**', component: AdminHomeComponent }
 ];
