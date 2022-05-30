d0260236b5974312d31c6190cb99052366762aaa
jossrc <kuro.rc10@gmail.com>
2022-05-30 01:17:28 -0500

    listado para guardar empleado

--- src/app/pages/crud-empleado/guardar-empleado/guardar-empleado.component.ts
+++ src/app/pages/crud-empleado/guardar-empleado/guardar-empleado.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
 import {EmpleadoService} from "../../../service/empleado.service";
+import {UbigeoService} from "../../../service/ubigeo.service";
+import {LocalService} from "../../../service/local.service";
+import {RolService} from "../../../service/rol.service";
+
+interface Departamento {
+  name: string,
+  code: string
+}
+
+interface Local {
+  name: string,
+  code: number
+}
+
+interface Rol {
+  name: string,
+  code: number
+}
 
 @Component({
   selector: 'app-guardar-empleado',
@@ -10,15 +28,25 @@ import {EmpleadoService} from "../../../service/empleado.service";
 export class GuardarEmpleadoComponent implements OnInit {
 
   // TODO: Arreglo de objetos de este tipo {name: 'New York', code: 'NY'},
-  listaDepartamentos = [];
+  listaDepartamentos : Departamento[] = [];
   listaProvincias = [];
   listaDistritos = [];
-  listaRoles = [];
-  listaLocales = [];
+  listaRoles: Rol[] = [];
+  listaLocales: Local[] = [];
 
-  constructor( private empleadoService:EmpleadoService, public ref: DynamicDialogRef, public config: DynamicDialogConfig) { }
+  constructor( private empleadoService:EmpleadoService,
+               private ubigeoService: UbigeoService,
+               private localService: LocalService,
+               private rolService: RolService,
+               public ref: DynamicDialogRef,
+               public config: DynamicDialogConfig) {
+
+  }
 
   ngOnInit(): void {
+    this.listarDepartamentos();
+    this.listarLocales();
+    this.listarRoles();
   }
 
   guardarEmpleado() {
@@ -30,5 +58,43 @@ export class GuardarEmpleadoComponent implements OnInit {
       console.log('Registro exitoso');
     });
   }
+
+  listarDepartamentos() {
+    this.ubigeoService.listarDepartamentos().subscribe( (departamentos: string[]) => {
+      const data = departamentos.map( (departamento) : Departamento=> {
+        return {
+          name: departamento,
+          code: departamento
+        }
+      });
+      this.listaDepartamentos = data;
+      console.log(this.listaDepartamentos)
+    })
+  }
+
+  listarLocales() {
+    this.localService.listarLocales().subscribe( (locales: any) => {
+      const data = locales.map( (local: any): Local => {
+        return {
+          name: local.nombre,
+          code: local.id
+        }
+      })
+      this.listaLocales = data;
+    });
+  }
+
+  listarRoles() {
+    this.rolService.listarRoles().subscribe( (roles: any) => {
+      const data = roles.filter((rol: any) => rol.id != 3).map( (rol: any): Rol => {
+        return {
+          name: rol.nombre.replace("ROLE_",""),
+          code: rol.id
+        }
+      })
+      this.listaRoles = data
+    })
+  }
+
 
 }
