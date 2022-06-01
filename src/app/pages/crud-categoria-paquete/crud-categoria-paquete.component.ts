9c6318433b8b0378f72176aa651e83f1166cc805
diegogu <diegogu@gmail.com>
2022-05-31 23:48:40 -0500

    Implementando listado categoria paquetes

--- src/app/pages/crud-categoria-paquete/crud-categoria-paquete.component.ts
+++ src/app/pages/crud-categoria-paquete/crud-categoria-paquete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ConfirmationService, MessageService} from "primeng/api";
+import {CategoriaPaqueteService} from "../../service/categoria-paquete.service";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
 
 @Component({
   selector: 'app-crud-categoria-paquete',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud-categoria-paquete.component.css']
 })
 export class CrudCategoriaPaqueteComponent implements OnInit {
+  selectCategoriaPaquetes = [];
+  listaCategoriaPaquetes = [];
 
-  constructor() { }
+  ref!: DynamicDialogRef;
+
+  constructor(
+    private categoriaPaqueteService:CategoriaPaqueteService,
+    public dialogService: DialogService,
+    public messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.obtenerCategoriaPaquete()
+  }
+
+  private obtenerCategoriaPaquete() {
+    this.categoriaPaqueteService.listarCategoriaPaquete().subscribe( (data: any) => {
+      this.listaCategoriaPaquetes = data.datos;
+      console.log(data);
+    });
   }
 
 }
--- src/app/service/categoria-paquete.service.ts
+++ src/app/service/categoria-paquete.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+
+const API_ROUTE = environment.API_ENDPOINT + "/categoriaPaquete";
+const token = environment.TOKEN_TEST
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaPaqueteService {
 
-  constructor() { }
+  private headers = new HttpHeaders()
+  .set("Content-type", "application/json")
+  .set("Authorization", token)
+
+  constructor(private http: HttpClient) { }
+
+  listarCategoriaPaquete(): Observable<any> {
+    return this.http.get(API_ROUTE + "/listar", { headers: this.headers } )
+  }
+
 }
