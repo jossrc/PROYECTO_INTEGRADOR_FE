20e601df70d4fa72a3c98b2509140e2a56a12246
Gushiken <i201911188@cibertec.edu.pe>
2022-06-02 14:55:59 -0500

    clonacion ok

--- src/app/service/cotizacion.service.ts
+++ src/app/service/cotizacion.service.ts
@@ -1,33 +1,4 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { HttpClient, HttpHeaders} from "@angular/common/http";
-import { Observable } from "rxjs";
-import { environment } from "../../environments/environment";
-
-const API_ROUTE = environment.API_ENDPOINT + "/usuarios/empleado";
-const token = environment.TOKEN_TEST
-
-@Injectable({
-    providedIn: 'root'
-  })
-  export class EmpleadoService {
-  
-    private headers = new HttpHeaders()
-      .set("Content-type", "application/json")
-      .set("Authorization", token)
-  
-    constructor( private http: HttpClient ) { }
-  
-    listarEmpleados(): Observable<any> {
-      return this.http.get(API_ROUTE + "/listar", { headers: this.headers } )
-    }
-  
-    registrarEmpleado(empleado: any): Observable<any> {
-      return this.http.post(API_ROUTE + "/registrar", empleado, { headers: this.headers } )
-    }
-  
-  }
-=======
 import {environment} from "../../environments/environment";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
@@ -45,4 +16,3 @@ export class CotizacionService {
 
   constructor( private http: HttpClient ) { }
 }
->>>>>>> 2b97978071dd6c223631656b816c683979941b2a
