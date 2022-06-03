0d8fa7b7047bf4ded6346044f6ee2dd63b2ea8e1
Gushiken <i201911188@cibertec.edu.pe>
2022-06-03 00:32:15 -0500

    avance de local front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  TOKEN_TEST: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJyb2xlXCI6XCJST0xFX0FETUlOXCJ9XSIsInN1YiI6ImRpZWdvZ3V0YXJyYUBnbWFpbC5jb20iLCJpYXQiOjE2NTQyMTU2NjEsImV4cCI6MTY1NDIyOTY2MX0.wlnuiW5bTh_pz1nOk1-ugQqOHR3enHQqs8-UeN14KBg",
+  TOKEN_TEST: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJyb2xlXCI6XCJST0xFX0FETUlOXCJ9XSIsInN1YiI6Imd1c2hpQGdtYWlsLmNvbSIsImlhdCI6MTY1NDIyOTg0NSwiZXhwIjoxNjU0MjQzODQ1fQ.aVZqFsgD18_qNYObNEbXs6-r6O8ZmrfgA3QzLjdcLhg",
   API_ENDPOINT: "http://localhost:8090/api"
 };
 
