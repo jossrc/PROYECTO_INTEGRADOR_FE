4e82de5d8c5816a2e4bb157115cc7ebe10a55956
José Robles Colonia <kuro.rc10@gmail.com>
2022-06-04 19:35:47 +0000

    inicio login y registro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  TOKEN_TEST: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJyb2xlXCI6XCJST0xFX0FETUlOXCJ9XSIsInN1YiI6Imd1c2hpQGdtYWlsLmNvbSIsImlhdCI6MTY1NDIyOTg0NSwiZXhwIjoxNjU0MjQzODQ1fQ.aVZqFsgD18_qNYObNEbXs6-r6O8ZmrfgA3QzLjdcLhg",
+  TOKEN_TEST: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJyb2xlXCI6XCJST0xFX0FETUlOXCJ9XSIsInN1YiI6ImFkbWluMkBnbWFpbC5jb20iLCJpYXQiOjE2NTQzNjQ3NzIsImV4cCI6MTY1NDM3ODc3Mn0.OdAJ4vV_CGZp4Lq5AkKyleZv-Rzf62jK8CGCUJm0kgw",
   API_ENDPOINT: "http://localhost:8090/api"
 };
 
