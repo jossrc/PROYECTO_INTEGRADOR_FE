c6db40ba648a0bdcc89fc3f116ee34f7b59b1718
jossrc <kuro.rc10@gmail.com>
2022-06-25 10:28:13 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  TOKEN_TEST: "eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6Ilt7XCJyb2xlXCI6XCJST0xFX0FETUlOXCJ9XSIsInN1YiI6Im1hcmlvMTIzNDVmTmg1ZkBnbWFpbC5jb20iLCJpYXQiOjE2NTU1NzM1MDYsImV4cCI6MTY1NTU4NzUwNn0.5pqHYn3uLfISlNQUQSyIS0Tad2b2mqmY07JdAbPcWFs",
+  API_ENDPOINT: "http://localhost:8090/api"
 };
